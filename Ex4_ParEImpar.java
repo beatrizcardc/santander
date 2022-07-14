93a08ea409ef4c0a2a6b5bb005b384f92be79e3a
beatrizcardc <beacarcun@gmail.com>
2022-07-14 13:56:00 -0300

    primeira parte do exercício

--- Ex4_ParEImpar.java
+++ Ex4_ParEImpar.java
@@ -1,4 +1,28 @@
 package br.com.dio.exercicios.Loops;
 
+import java.util.Scanner;
+/*4º Exercício Do While
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números impares.
+*/
+
 public class Ex4_ParEImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantidadeNumeros;
+        int numero;
+
+        System.out.println("Quantidade de números: ");
+        quantidadeNumeros = scan.nextInt(); //primeira parte do exercício - pedirá qtas vezes ingressaremos um número
+        int count = 0; // inicia o contador
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();//solicita um número até que atinja a qtd informada.
+            count ++; //incrementa o contador - senão ficaria sempre no zero e sempre o count zero seria menos que a qtd de números.
+
+        } while(count < quantidadeNumeros); // a repetição acontecerá até atingir a qtd de nºs informada
+
+    }
 }
